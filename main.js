f1ec2f23ffda6c68a495aac506e8f66f02181d8e
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-07-16 17:13:24 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const username = prompt('Ismingizni kiriting');
+const username1 = prompt('Familliyangizni kiriting');
+const username2 = prompt('Yoshingizni kiriting');
+const username3 = prompt('Yoqtirgaan faningizni yozing');
+const username4 = prompt('Yashaydigan tumaningizni kiriting');
+
+alert("malumotnomangizni kiritgan uchun rahmat");
+
+console.log(username, username1, username2, username3, username4, username5, alert);
+
+
